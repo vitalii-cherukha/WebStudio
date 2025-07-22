2430121871109afe4f71d0456af4510c1357c893
vitalii <vitalii.cherukha@gmail.com>
2025-07-22 15:39:02 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const refs = {
   openModal: document.querySelector('.hero-button'),
   backBtn: document.querySelector('.modal-button-exit'),
   modalOvarlay: document.querySelector('.modal-overlay'),
+  burgerBtn: document.querySelector('.burger-menu'),
+  mobileBackBtn: document.querySelector('.mobile-button-exit'),
+  mobileModal: document.querySelector('.menu-overlay'),
 };
 
 const onModalIsOpenClick = e => {
@@ -18,6 +21,16 @@ const onModalIsHideBtnClick = e => {
   refs.modalOvarlay.classList.remove('is-open');
 };
 
+const onShowMobileModalClick = e => {
+  refs.mobileModal.classList.add('is-open');
+};
+
+const onMobileModalIsHideBtn = e => {
+  refs.mobileModal.classList.remove('is-open');
+};
+
 refs.openModal.addEventListener('click', onModalIsOpenClick);
 refs.backBtn.addEventListener('click', onModalIsHideBtnClick);
 refs.modalOvarlay.addEventListener('click', onModalIsHideClick);
+refs.burgerBtn.addEventListener('click', onShowMobileModalClick);
+refs.mobileBackBtn.addEventListener('click', onMobileModalIsHideBtn);
